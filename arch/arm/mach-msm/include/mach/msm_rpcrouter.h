6f7b44c6c400d8af2aef99db2ec519c8dfd8df05
DafabHoid <fabian@dafabhoid.de>
2018-02-04 20:44:35 +0100

    Added MSM rtc clock support
    Source: https://github.com/AriesVE-DevCon-TEAM/samsung-kernel-msm7x30

--- arch/arm/mach-msm/include/mach/msm_rpcrouter.h
+++ arch/arm/mach-msm/include/mach/msm_rpcrouter.h
@@ -21,6 +21,34 @@
 #include <linux/types.h>
 #include <linux/list.h>
 #include <linux/platform_device.h>
+#include <linux/ioctl.h>
+
+#define RPC_ROUTER_VERSION_V1 0x00010000
+
+struct rpcrouter_ioctl_server_args {
+	uint32_t prog;
+	uint32_t vers;
+};
+
+#define RPC_ROUTER_IOCTL_MAGIC (0xC1)
+
+#define RPC_ROUTER_IOCTL_GET_VERSION \
+	_IOR(RPC_ROUTER_IOCTL_MAGIC, 0, unsigned int)
+
+#define RPC_ROUTER_IOCTL_GET_MTU \
+	_IOR(RPC_ROUTER_IOCTL_MAGIC, 1, unsigned int)
+
+#define RPC_ROUTER_IOCTL_REGISTER_SERVER \
+	_IOWR(RPC_ROUTER_IOCTL_MAGIC, 2, unsigned int)
+
+#define RPC_ROUTER_IOCTL_UNREGISTER_SERVER \
+	_IOWR(RPC_ROUTER_IOCTL_MAGIC, 3, unsigned int)
+
+#define RPC_ROUTER_IOCTL_CLEAR_NETRESET \
+	_IOWR(RPC_ROUTER_IOCTL_MAGIC, 4, unsigned int)
+
+#define RPC_ROUTER_IOCTL_GET_CURR_PKT_SIZE \
+	_IOR(RPC_ROUTER_IOCTL_MAGIC, 5, unsigned int)
 
 /* RPC API version structure
  * Version bit 31 : 1->hashkey versioning,
